f75a0eb59404174b1025ae84700672b9f3064695
Devika Baburaj <31156213+DevikaBaburaj@users.noreply.github.com>
2023-01-10 14:56:28 +0530

    ..

--- Day2/aws_ec2_defVPC/variable.tf
+++ Day2/aws_ec2_defVPC/variable.tf
@@ -1,11 +1,3 @@
-output "public_ip" {
-   value = aws_instance.web.public_ip
-}
-
-output "private_ip" {
-   value =aws_instance.web.private_ip
-}
-root@DESKTOP-1V19J76:/mnt/f/KEYSHELL/Keyshell_Terraform/Day2/aws_ec2_defVPC# cat variable.tf
 variable "instance_type" {
    type = string
    default = "t2.micro"
