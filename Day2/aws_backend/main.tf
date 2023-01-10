20ae74971aefc9674e4c6400ffeaf4efbb2e19f0
Devika Baburaj <31156213+DevikaBaburaj@users.noreply.github.com>
2023-01-10 13:23:27 +0530

    created main.tf

---
+++ Day2/aws_backend/main.tf
@@ -0,0 +1,22 @@
+# terraform {
+#    backend "s3" {
+#      bucket = "terraform-backend-keyshell"
+#      key = "terraform.tfstate"
+#      region = "us-east-1"
+#      dynamodb_table = "terraform"
+#    }
+# }
+
+
+resource "aws_instance" "example"{
+    ami = "ami-026b57f3c383c2eec"
+    instance_type = var.instance_type
+    associate_public_ip_address = var.public_ip
+
+    tags = var.tags
+}
+
+resource "aws_iam_user" "name" {
+  count = length(var.name)
+  name = var.name[count.index]
+}
