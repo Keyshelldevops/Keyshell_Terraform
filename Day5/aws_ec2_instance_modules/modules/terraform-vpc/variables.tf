fc8548712c990f1923eeb6c4a347bef6e90d4493
Devika Baburaj <31156213+DevikaBaburaj@users.noreply.github.com>
2023-01-13 08:22:16 +0530

    created variables.tf

---
+++ Day5/aws_ec2_instance_modules/modules/terraform-vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_cidr_block" {
+   type = string
+}
+
+variable "pub_cidr_block" {
+   type = list(string)
+}
+
+variable "az" {
+   type = list(string)
+}
+
+variable "priv_cidr_block" {
+   type = list(string)
+}
+
+variable "nat_gateway_count" {
+   type = number
+}
+
+variable "enable_dns_hostnames" {
+   type = bool
+}
+
+variable "enable_dns_support" {
+   type = bool
+}
